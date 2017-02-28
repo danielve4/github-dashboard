2f3f3bb222507ced011c793c5ac6d39b348fa819
Daniel Vega <danve4@gmail.com>
2017-02-28 04:44:17 -0600

    Added testing tags dynamically

--- site.js
+++ site.js
@@ -34,23 +34,58 @@
         } else if(response.status === 202) {
           window.setTimeout(function () {
             getRepoStats(repoUrl, callback);
-          }, 2000);
+          }, 3000);
         }
       });
   }
 
-  var openIssuesCount = document.querySelector('#open-issues-count');
-  var watchersCount = document.querySelector('#watchers-count');
-  var forksCount = document.querySelector('#forks-count');
-  getRepoInfo('repo-json-ex.json', function (json) {
-    openIssuesCount.innerHTML = json.open_issues_count;
-    watchersCount.innerHTML = json.subscribers_count;
-    forksCount.innerHTML = json.forks_count;
-    console.log(json);
-  });
+  // var openIssuesCount = document.querySelector('#open-issues-count');
+  // var watchersCount = document.querySelector('#watchers-count');
+  // var forksCount = document.querySelector('#forks-count');
+  // var commitCount = document.querySelector('#commits-count');
+  function addRepoInfoToDoc(repoUrl, statsUrl) {
+    var mainUl = document.querySelector('#all-repos-info');
+    var mainUlLi = document.createElement('li');
+    mainUl.appendChild(mainUlLi);
+    var ulStats = document.createElement('ul');
+    ulStats.className = 'card';
 
-  // getRepoInfo('repo-json-ex.json',function (json) {
-  //   console.log(json);
-  // });
+    var openIssuesCount = document.createElement('li');
+    var watchersCount = document.createElement('li');
+    var forksCount = document.createElement('li');
+    var commitCount = document.createElement('li');
+
+    getRepoInfo(repoUrl, function (json) {
+      openIssuesCount.innerHTML = 'Open Issues: '+json.open_issues_count;
+      ulStats.appendChild(openIssuesCount);
+      watchersCount.innerHTML = 'Watchers: '+json.subscribers_count;
+      ulStats.appendChild(watchersCount);
+      forksCount.innerHTML = 'Forks: '+json.forks_count;
+      ulStats.appendChild(forksCount);
+      console.log(json);
+    });
+    getRepoInfo(statsUrl,function (json) {
+      var sumCommits = json.all.reduce(function (a, b) {return a+b},0);
+      commitCount.innerHTML = 'Commits: '+sumCommits;
+      ulStats.appendChild(commitCount);
+      console.log(json);
+    });
+    mainUlLi.appendChild(ulStats);
+  }
+
+  function addAllInfoToDoc() {
+    var stats = 'stats-json-ex.json';
+    var repo = 'repo-json-ex.json';
+
+    addRepoInfoToDoc(repo, stats);
+  }
+
+  for(var i=0;i<2;i++) {
+    (function(i){
+      window.setTimeout(function(){
+        addAllInfoToDoc()
+      }, i * 5000);
+    }(i));
+  }
 
 })();
